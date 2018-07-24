a911913294965c0c4e013b01573c8af93d9c2e89
kimeuichan <eck7965@gmail.com>
2018-07-24 16:07:42 +0900

    외부 페이지 테스트

--- fittingroom.php
+++ fittingroom.php
@@ -2,11 +2,11 @@
 
 include_once('./_common.php');
 
-include_once(G5_SHOP_PATH.'/shop.head.php');
 
 include_once (THEMA_PATH.'/shop.index.php');
 
 echo $member[mb_nick];
+echo "test";
 
 
 include_once(G5_SHOP_PATH.'/shop.tail.php');
